0a22884a564b838f1fea8d909a9f3baf1891c2ee
dana-savinova <savinovadana@ya.ru>
2019-04-22 19:22:59 +0300

    Верстает декоративные элементы и попап в index.html

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
 var headerNav = document.querySelector('.page-header__nav');
 var headerToggle = document.querySelector('.page-header__toggle');
+var promoBtn = document.querySelector('.promo__button');
+var popUpOverlay = document.querySelector('.modal-overlay');
+var popUp = document.querySelector('.modal-cart');
 
 headerNav.classList.remove('page-header__nav--nojs');
 // Открытие меню
@@ -12,3 +15,26 @@ headerToggle.addEventListener('click', function() {
       headerNav.classList.remove('page-header__nav--opened');
     }
   });
+
+//Открытие попапа
+promoBtn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popUpOverlay.classList.add('modal-overlay--show');
+});
+
+popUpOverlay.addEventListener('click', function() {
+  popUpOverlay.classList.remove('modal-overlay--show');
+});
+
+popUp.addEventListener('click', function (evt) {
+  evt.stopPropagation();
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popUpOverlay.classList.contains('modal-overlay--show')) {
+      popUpOverlay.classList.remove('modal-overlay--show');
+    }
+  }
+});
